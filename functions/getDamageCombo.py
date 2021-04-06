586c6f0c8e531ebebecb29af4ae4f7c8c4515e45
MasterTemple <dgmastertemple@protonmail.com>
2021-04-06 13:40:38 -0700

    damage tree works somehow lol

--- functions/getDamageCombo.py
+++ functions/getDamageCombo.py
@@ -1,4 +1,7 @@
 import sqlite3
+import os
+import json
+
 def create_connection(db_file):
     conn = None
     try:
@@ -7,10 +10,78 @@ def create_connection(db_file):
         print("\nError")
     return conn
 
-file = "work/cdclient.sqlite"
+
+def writeFile(data):
+
+    #os.makedirs(os.path.dirname('test.json'), exist_ok=True)
+    with open('test.json', 'w',
+              encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+file = "./../work/cdclient.sqlite"
 db = create_connection(file)
+
+actions = ["action", "miss action", "blocked action", "action_false", "action_true", "start_action", "chain_action", "break_action", "double_jump_action", "ground_action", "jump_action", "hit_action", "hit_action_enemy", "timeout_action", "air_action", "falling_action", "jetpack_action", "spawn_fail_action", "action_failed", "action_consumed", "blocked_action", "moving_action", "on_success", "behavior","behavior 0","behavior 1","behavior 2","behavior 3","behavior 4","behavior 5","behavior 6","behavior 7","behavior 8","behavior 9","bahavior 2"]
+
 cur = db.cursor()
-cur.execute("SELECT * FROM SkillBehavior"),
+cur.execute("SELECT * FROM BehaviorParameter"),
 rows = cur.fetchall()
+behaviorID = 25981
+obj = {}
+used = []
 for row in rows:
-    pass
+    if row[0] == behaviorID:
+        #print(row[1])
+        obj[row[1]] = {"initial_value": int(row[2])}
+    #pass
+
+#print(obj)
+
+def getKidsKids(obj, parameter):
+    obj[parameter] = {}
+    obj[parameter]['hasKids'] = False
+    obj[parameter]['info'] = {}
+    obj[parameter]['kids'] = {}
+    #print(obj[parameter]['initial_value'])
+    #print(parameter)
+
+    for row in rows:
+        if row[0] == int(parameter):
+            obj[parameter]['hasKids'] = True
+            obj[parameter]['info'][row[1]] = int(row[2])
+            #obj[parameter]['kids'][row[1]] = int(row[2])
+            #print(obj)
+
+
+    for param in obj[parameter]['info']:
+        if param in actions and obj[parameter]['info'][param] not in used:
+            print(obj[parameter]['kids'])
+            used.append(obj[parameter]['info'][param])
+            getKidsKids(obj[parameter]['kids'], int(obj[parameter]['info'][param]))
+            # pass
+            # else:
+                #getKidsKids(obj[parameter]['kids'], int(row[2]), True)
+                # pass
+                #obj[parameter]['kids'][str(row[0])]
+    #used.append(parameter)
+    return obj
+
+
+for parameter in obj:
+    obj[parameter]['hasKids'] = False
+    obj[parameter]['kids'] = {}
+    #print(obj[parameter]['initial_value'])
+    for row in rows:
+        if row[0] == obj[parameter]['initial_value']:
+            obj[parameter]['hasKids'] = True
+            obj[parameter]['kids'][str(row[0])] = {row[1]: int(row[2])}
+            if row[1] in actions:
+                getKidsKids(obj[parameter]['kids'][str(row[0])], int(row[2]))
+
+
+
+
+
+#print(obj)
+
+writeFile(obj)
