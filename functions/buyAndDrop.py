5003e640126eb35b3382f44d4b7a2f0c203d9161
MasterTemple <dgmastertemple@protonmail.com>
2021-05-27 17:34:09 -0800

    sql should be much faster on objects

--- functions/buyAndDrop.py
+++ functions/buyAndDrop.py
@@ -32,35 +32,34 @@ def getLTI(conn, data, objectID):
 
 def getLMI(conn, data):
     cur = conn.cursor()
-    cur.execute("SELECT * FROM LootMatrix")
+    cur.execute("SELECT * FROM LootMatrix WHERE LootTableIndex IN ("+ ",".join(["?" for _ in data['buyAndDrop']['LootTableIndexes']]) + ")", data['buyAndDrop']['LootTableIndexes'])
     rows = cur.fetchall()
     for row in rows:
-        if row[1] in data['buyAndDrop']['LootTableIndexes']:
-            data['buyAndDrop']['LootMatrixIndexesArray'].append(row[0])
-            obj = {
-                "LootMatrixIndex": row[0],
-                "LootTableIndex": row[1],
-                "RarityTableIndex": row[2],
-                "percent": round(row[3] * 100, 2),
-                "minToDrop": row[4],
-                "maxToDrop": row[5],
-                "DestructibleComponent": {}
-            }
-            data['buyAndDrop']['LootMatrixIndexes'][row[0]] = obj
-            data['buyAndDrop']['LootMatrixIndexes'][row[0]]['rarityTableInfo'] = {}
-            data = rarityTable(conn, data, row[0], row[2])
-            # data['buyAndDrop']['LootMatrixIndexes'].append(obj)
+
+        data['buyAndDrop']['LootMatrixIndexesArray'].append(row[0])
+        obj = {
+            "LootMatrixIndex": row[0],
+            "LootTableIndex": row[1],
+            "RarityTableIndex": row[2],
+            "percent": round(row[3] * 100, 2),
+            "minToDrop": row[4],
+            "maxToDrop": row[5],
+            "DestructibleComponent": {}
+        }
+        data['buyAndDrop']['LootMatrixIndexes'][row[0]] = obj
+        data['buyAndDrop']['LootMatrixIndexes'][row[0]]['rarityTableInfo'] = {}
+        data = rarityTable(conn, data, row[0], row[2])
+        # data['buyAndDrop']['LootMatrixIndexes'].append(obj)
 
     return data
 
 
 def inNPC(conn, data):
     cur = conn.cursor()
-    cur.execute("SELECT * FROM LootMatrixIndex")
+    cur.execute("SELECT * FROM LootMatrixIndex WHERE LootMatrixIndex IN ("+",".join(["?" for _ in data['buyAndDrop']['LootMatrixIndexesArray']])+")", data['buyAndDrop']['LootMatrixIndexesArray'])
     rows = cur.fetchall()
     for row in rows:
-        if row[0] in data['buyAndDrop']['LootMatrixIndexesArray']:
-            data['buyAndDrop']['LootMatrixIndexes'][row[0]]['inNpcEditor'] = row[1]
+        data['buyAndDrop']['LootMatrixIndexes'][row[0]]['inNpcEditor'] = row[1]
 
     return data
 
@@ -68,11 +67,10 @@ def inNPC(conn, data):
 def getVendorComponents(conn, data):
     cur = conn.cursor()
     data['buyAndDrop']['VendorComponents'] = []
-    cur.execute("SELECT id, LootMatrixIndex FROM VendorComponent")
+    cur.execute("SELECT id, LootMatrixIndex FROM VendorComponent WHERE LootMatrixIndex IN ("+ ",".join(["?" for _ in data['buyAndDrop']['LootMatrixIndexesArray']]) + ")", data['buyAndDrop']['LootMatrixIndexesArray'])
     rows = cur.fetchall()
     for row in rows:
-        if row[1] in data['buyAndDrop']['LootMatrixIndexesArray']:
-            data['buyAndDrop']['VendorComponents'].append(row[0])
+        data['buyAndDrop']['VendorComponents'].append(row[0])
 
     return data
 
@@ -80,11 +78,10 @@ def getVendorComponents(conn, data):
 def getVendorIDs(conn, data):
     cur = conn.cursor()
     data['buyAndDrop']['VendorIDs'] = []
-    cur.execute("SELECT * FROM ComponentsRegistry")
+    cur.execute("SELECT * FROM ComponentsRegistry WHERE component_id IN ("+ ",".join(["?" for _ in data['buyAndDrop']['VendorComponents']]) + ") AND component_type=16", data['buyAndDrop']['VendorComponents'])
     rows = cur.fetchall()
     for row in rows:
-        if row[2] in data['buyAndDrop']['VendorComponents'] and row[1] == 16:
-            data['buyAndDrop']['VendorIDs'].append(row[0])
+        data['buyAndDrop']['VendorIDs'].append(row[0])
 
     return data
 
@@ -92,16 +89,15 @@ def getVendorIDs(conn, data):
 def getVendorNames(conn, data):
     cur = conn.cursor()
     data['buyAndDrop']['Vendors'] = []
-    cur.execute("SELECT id, name, displayName FROM Objects")
+    cur.execute("SELECT id, name, displayName FROM Objects WHERE id IN ("+ ",".join(["?" for _ in data['buyAndDrop']['VendorIDs']]) + ")", data['buyAndDrop']['VendorIDs'])
     rows = cur.fetchall()
     for row in rows:
-        if row[0] in data['buyAndDrop']['VendorIDs']:
-            obj = {
-                "id": row[0],
-                "name": row[1],
-                "displayName": row[2]
-            }
-            data['buyAndDrop']['Vendors'].append(obj)
+        obj = {
+            "id": row[0],
+            "name": row[1],
+            "displayName": row[2]
+        }
+        data['buyAndDrop']['Vendors'].append(obj)
     return data
 
 
@@ -115,27 +111,26 @@ def getDestructibleComponents(conn, data):
     #packages = [13102]
     cur = conn.cursor()
     data['buyAndDrop']['DestructibleComponents'] = []
-    cur.execute("SELECT id, LootMatrixIndex FROM DestructibleComponent")
+    cur.execute("SELECT id, LootMatrixIndex FROM DestructibleComponent WHERE LootMatrixIndex IN ("+ ",".join(["?" for _ in data['buyAndDrop']['LootMatrixIndexesArray']]) + ")", data['buyAndDrop']['LootMatrixIndexesArray'])
     rows = cur.fetchall()
     for row in rows:
-        if row[1] in data['buyAndDrop']['LootMatrixIndexesArray']:
-            data['buyAndDrop']['DestructibleComponents'].append(row[0])
-            #print(row[1], row[0])
-            #if row[1] not in data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']:
-            #if row[1] not in data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']:
-            enemyID = getOneEnemyID(conn, data, row[0])
-            #print(enemyID)
+        data['buyAndDrop']['DestructibleComponents'].append(row[0])
+        #print(row[1], row[0])
+        #if row[1] not in data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']:
+        #if row[1] not in data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']:
+        enemyID = getOneEnemyID(conn, data, row[0])
+        #print(enemyID)
 
-            if enemyID in implementedEnemies:
-                data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent'][row[0]] = {
-                    "enemyID": enemyID,
-                    "enemyNames": getOneEnemyName(conn, data, enemyID)
-                }
-            #data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyNames'] = getOneEnemyName(conn, data, data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyID'])
-            #if True:
-               # data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['comp_val'] = (row[0])
-               # data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyID'] = getOneEnemyID(conn, data, row[0])
-               # data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyNames'] = getOneEnemyName(conn, data, data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyID'])
+        if enemyID in implementedEnemies:
+            data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent'][row[0]] = {
+                "enemyID": enemyID,
+                "enemyNames": getOneEnemyName(conn, data, enemyID)
+            }
+        #data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyNames'] = getOneEnemyName(conn, data, data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyID'])
+        #if True:
+           # data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['comp_val'] = (row[0])
+           # data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyID'] = getOneEnemyID(conn, data, row[0])
+           # data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyNames'] = getOneEnemyName(conn, data, data['buyAndDrop']['LootMatrixIndexes'][row[1]]['DestructibleComponent']['enemyID'])
     return data
 
 
@@ -154,11 +149,11 @@ def getEnemyIDs(conn, data):
     implementedEnemies = enemiesFile['used']
     cur = conn.cursor()
     data['buyAndDrop']['EnemyIDs'] = []
-    cur.execute("SELECT * FROM ComponentsRegistry")
+    cur.execute("SELECT * FROM ComponentsRegistry WHERE component_type=7 AND component_id IN ("+ ",".join(["?" for _ in data['buyAndDrop']['DestructibleComponents']]) + ") AND id IN ("+ ",".join(["?" for _ in implementedEnemies]) + ")", (data['buyAndDrop']['DestructibleComponents'], implementedEnemies))
     rows = cur.fetchall()
     for row in rows:
-        if row[2] in data['buyAndDrop']['DestructibleComponents'] and row[1] == 7 and row[0] in implementedEnemies:
-            data['buyAndDrop']['EnemyIDs'].append(row[0])
+        #if row[2] in data['buyAndDrop']['DestructibleComponents'] and row[1] == 7 and row[0] in implementedEnemies:
+        data['buyAndDrop']['EnemyIDs'].append(row[0])
 
     return data
 
@@ -166,16 +161,16 @@ def getEnemyIDs(conn, data):
 def getEnemyNames(conn, data):
     cur = conn.cursor()
     data['buyAndDrop']['Enemies'] = []
-    cur.execute("SELECT id, name, displayName FROM Objects")
+    cur.execute("SELECT id, name, displayName FROM Objects WHERE id IN ("+ ",".join(["?" for _ in data['buyAndDrop']['EnemyIDs']]) + ")", data['buyAndDrop']['EnemyIDs'])
     rows = cur.fetchall()
     for row in rows:
-        if row[0] in data['buyAndDrop']['EnemyIDs']:
-            obj = {
-                "id": row[0],
-                "name": row[1],
-                "displayName": row[2]
-            }
-            data['buyAndDrop']['Enemies'].append(obj)
+        #if row[0] in data['buyAndDrop']['EnemyIDs']:
+        obj = {
+            "id": row[0],
+            "name": row[1],
+            "displayName": row[2]
+        }
+        data['buyAndDrop']['Enemies'].append(obj)
     return data
 
 
@@ -189,12 +184,11 @@ def getOneEnemyID(conn, data, dComp):
 
     cur = conn.cursor()
     data['buyAndDrop']['EnemyIDs'] = []
-    cur.execute("SELECT * FROM ComponentsRegistry")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[2] == dComp and row[1] == 7 and row[0] in implementedEnemies:
-            #data['buyAndDrop']['EnemyIDs'].append(row[0])
-            return row[0]
+    cur.execute("SELECT * FROM ComponentsRegistry WHERE component_type=7 AND component_id=? AND id IN ("+ ",".join(["?" for _ in implementedEnemies]) + ")", (dComp, implementedEnemies))
+    row = cur.fetchone()
+    #if row[2] == dComp and row[1] == 7 and row[0] in implementedEnemies:
+        #data['buyAndDrop']['EnemyIDs'].append(row[0])
+    return row[0]
 
         #return data
 
@@ -203,40 +197,39 @@ def getOneEnemyID(conn, data, dComp):
 def getOneEnemyName(conn, data, objectID):
     cur = conn.cursor()
     data['buyAndDrop']['Enemies'] = []
-    cur.execute("SELECT id, name, displayName FROM Objects")
-    rows = cur.fetchall()
-    for row in rows:
-        if row[0] == objectID:
-            obj = {
-                "name": row[1],
-                "displayName": row[2]
-            }
-            return obj
+    cur.execute("SELECT id, name, displayName FROM Objects WHERE id=?", (objectID,))
+    row = cur.fetchone()
+        # if row[0] == objectID:
+    obj = {
+        "name": row[1],
+        "displayName": row[2]
+    }
+    return obj
 
 
 def rarityTable(conn, data, lmi, rti):
     cur = conn.cursor()
-    cur.execute("SELECT * FROM RarityTable")
+    cur.execute("SELECT * FROM RarityTable WHERE RarityTableIndex=?", (rti,))
     rows = cur.fetchall()
     for row in rows:
-        if row[3] == rti:
-            obj = {
-                "id": row[0],
-                "randmax": round(row[1] * 100, 2),
-                "rarity": row[2]
-            }
-            #print(rti)
-            #print(data['buyAndDrop']['LootMatrixIndexes'])
-            data['buyAndDrop']['LootMatrixIndexes'][lmi]['rarityTableInfo'][row[2]] = obj
+        #if row[3] == rti:
+        obj = {
+            "id": row[0],
+            "randmax": round(row[1] * 100, 2),
+            "rarity": row[2]
+        }
+        #print(rti)
+        #print(data['buyAndDrop']['LootMatrixIndexes'])
+        data['buyAndDrop']['LootMatrixIndexes'][lmi]['rarityTableInfo'][row[2]] = obj
 
-            import json
-            with open('work/config.json') as f:
-                config = json.load(f)
-            with open(config['path']+'/lootTableIndexes/'+str(data['buyAndDrop']['LootMatrixIndexes'][lmi]['LootTableIndex'])+'.json') as f:
-                rarityCount = json.load(f)
+        import json
+        with open('work/config.json') as f:
+            config = json.load(f)
+        with open(config['path']+'/lootTableIndexes/'+str(data['buyAndDrop']['LootMatrixIndexes'][lmi]['LootTableIndex'])+'.json') as f:
+            rarityCount = json.load(f)
 
-            data['buyAndDrop']['LootMatrixIndexes'][lmi]['rarityCount'] = rarityCount['rarityCount']
+        data['buyAndDrop']['LootMatrixIndexes'][lmi]['rarityCount'] = rarityCount['rarityCount']
 
-            # data['buyAndDrop']['LootMatrixIndexes'].append(obj)
+        # data['buyAndDrop']['LootMatrixIndexes'].append(obj)
 
     return data
--- functions/earn.py
+++ functions/earn.py
@@ -7,33 +7,33 @@ def getInfo(conn, data, objectID):
 
     cur = conn.cursor()
     #    cur.execute("SELECT id, defined_type, defined_subtype, isChoiceReward, reward_item1, reward_item1_count, reward_item2, reward_item2_count, reward_item3, reward_item3_count, reward_item4, reward_item4_count FROM ComponentsRegistry")
-    cur.execute("SELECT id, defined_type, defined_subtype, isChoiceReward, reward_item1, reward_item1_count, reward_item2, reward_item2_count, reward_item3, reward_item3_count, reward_item4, reward_item4_count, isMission FROM Missions")
+    cur.execute("SELECT id, defined_type, defined_subtype, isChoiceReward, reward_item1, reward_item1_count, reward_item2, reward_item2_count, reward_item3, reward_item3_count, reward_item4, reward_item4_count, isMission FROM Missions WHERE reward_item1=? OR reward_item2=? OR reward_item3=? OR reward_item4=?", (objectID, objectID, objectID, objectID))
 
     rows = cur.fetchall()
 
     for row in rows:
         try:
-            if row[4] == objectID or row[6] == objectID or row[8] == objectID or row[10] == objectID:
-                if row[12] == 1:
-                    missionData = missionInfo.getMissionInfo(row[0], root)
-                if row[12] == 0:
-                    missionData = missionInfo.getAchievementInfo(row[0], root)
-                data['earn'][row[0]] = {
-                    "defined_type": row[1],
-                    "defined_subtype": row[2],
-                    "missionName": missionData['name'],
-                    "missionDescription": missionData['description'],
-                    "isMission": row[12]
-                }
+            # if row[4] == objectID or row[6] == objectID or row[8] == objectID or row[10] == objectID:
+            if row[12] == 1:
+                missionData = missionInfo.getMissionInfo(row[0], root)
+            if row[12] == 0:
+                missionData = missionInfo.getAchievementInfo(row[0], root)
+            data['earn'][row[0]] = {
+                "defined_type": row[1],
+                "defined_subtype": row[2],
+                "missionName": missionData['name'],
+                "missionDescription": missionData['description'],
+                "isMission": row[12]
+            }
 
-                if objectID == row[4]:
-                    data['earn'][row[0]]['rewardCount'] = row[5]
-                if objectID == row[6]:
-                    data['earn'][row[0]]['rewardCount'] = row[7]
-                if objectID == row[8]:
-                    data['earn'][row[0]]['rewardCount'] = row[9]
-                if objectID == row[10]:
-                    data['earn'][row[0]]['rewardCount'] = row[11]
+            if objectID == row[4]:
+                data['earn'][row[0]]['rewardCount'] = row[5]
+            if objectID == row[6]:
+                data['earn'][row[0]]['rewardCount'] = row[7]
+            if objectID == row[8]:
+                data['earn'][row[0]]['rewardCount'] = row[9]
+            if objectID == row[10]:
+                data['earn'][row[0]]['rewardCount'] = row[11]
         except:
             pass
     return data
